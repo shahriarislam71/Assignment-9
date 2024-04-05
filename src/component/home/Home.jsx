af17f36619f6f9a261e45d554006515005c8626b
shahriarislam <shahriarislamrafi71@gmail.com>
2024-04-06 03:02:45 +0600

    re-added image

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -30,9 +30,9 @@ const Home = () => {
                         <span className='flex  md:block justify-center'><button className='btn '>Get Started </button></span>
 
                     </div>
-                    <div className='relative lg:absolute end-0 max-w-screen-sm pb-5 pt-5 md:pt-0'>
+                    <div className='  pt-5 md:pt-0'>
                         <div className='lg:ml-auto'>
-                            <img className=' object-cover md:img-height' src="../../../public/profile.png" alt="" />
+                            <img className=' object-cover md:img-height' src="https://img.freepik.com/free-photo/handsome-bearded-guy-posing-against-white-wall_273609-20597.jpg?size=626&ext=jpg&ga=GA1.1.34264412.1712275200&semt=sph" alt="" />
                         </div>
                     </div>
                 </div>
@@ -43,22 +43,22 @@ const Home = () => {
                 <p className='text-center mt-6 text-gray-500 px-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
                 <div className='flex flex-col md:flex-row my-container px-5 justify-between mb-4 mt-8 rounded-lg gap-5'>
                     <div className='backgrondd pl-10 pt-10'>
-                        <img className='pb-8' src="../../../public/accounts 1.png" alt="" />
+                        <img className='pb-8 w-16' src="https://cdn3.vectorstock.com/i/1000x1000/03/92/accounting-and-financial-logo-vector-28280392.jpg" alt="" />
                         <h1 className='pr-20 pb-4 text-xl font-extrabold'>Account & Finance</h1>
                         <p className='pb-10'>300 Jobs Available</p>
                     </div>
                     <div className='backgrondd pl-10 pt-10'>
-                        <img className='pb-8' src="../../../public/business 1.png" alt="" />
+                        <img className='pb-8 w-16' src="https://img.freepik.com/premium-vector/creative-unique-pencil-art-logo-design-template_132230-556.jpg" alt="" />
                         <h1 className='pr-20 pb-4 text-xl font-extrabold'>Creative Design</h1>
                         <p className='pb-10'>100+ Jobs Available</p>
                     </div>
                     <div className='backgrondd pl-10 pt-10'>
-                        <img className='pb-8' src="../../../public/chip 1.png" alt="" />
+                        <img className='pb-8 w-16' src="https://c8.alamy.com/comp/2CA4EPT/marketing-finance-sales-economical-business-solutions-logo-vector-illustration-2CA4EPT.jpg" alt="" />
                         <h1 className='pr-20 pb-4 text-xl font-extrabold'>Marketing & Sales</h1>
                         <p className='pb-10'>150 Jobs Available</p>
                     </div>
                     <div className='backgrondd pl-10 pt-10'>
-                        <img className='pb-8' src="../../../public/social-media 1.png" alt="" />
+                        <img className='pb-8 w-16' src="https://www.shutterstock.com/image-vector/engineer-logo-template-design-vector-260nw-652938451.jpg" alt="" />
                         <h1 className='pr-20 pb-4 text-xl font-extrabold'>Engineering Job</h1>
                         <p className='pb-10'>224 Jobs Available</p>
                     </div>
